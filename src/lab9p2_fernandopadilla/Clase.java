a5c9e3758fadf51edec993144419acfeadb8758d
Lloyd Cooper <cooperc913@gmail.com>
2023-06-16 15:53:33 -0600

    commit clases

---
+++ src/lab9p2_fernandopadilla/Clase.java
@@ -0,0 +1,23 @@
+package lab9p2_fernandopadilla;
+
+public class Clase extends Asignatura {
+    private double costo;
+
+    public Clase(double costo, String codigoAsignatura, String codigoSeccion, String horario, String docente, int unidadesValorativas, String aula, String edificio, int cantidadAlumnos, String modalidad, String diasImparte, double precio) {
+        super(codigoAsignatura, codigoSeccion, horario, docente, unidadesValorativas, aula, edificio, cantidadAlumnos, modalidad, diasImparte, precio);
+        this.costo = 0.20;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    @Override
+    public String toString() {
+        return "Clase{" + "costo=" + costo + '}';
+    }
+}
---
+++ src/lab9p2_fernandopadilla/Laboratorio.java
@@ -0,0 +1,25 @@
+package lab9p2_fernandopadilla;
+
+public class Laboratorio extends Asignatura {
+    private double costo;
+
+    public Laboratorio(double costo, String codigoAsignatura, String codigoSeccion, String horario, String docente, int unidadesValorativas, String aula, String edificio, int cantidadAlumnos, String modalidad, String diasImparte, double precio) {
+        super(codigoAsignatura, codigoSeccion, horario, docente, unidadesValorativas, aula, edificio, cantidadAlumnos, modalidad, diasImparte, precio);
+        this.costo = 0.036;;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    @Override
+    public String toString() {
+        return "Laboratorio{" + "costo=" + costo + '}';
+    }
+
+    
+}
